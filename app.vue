5e2f79a850dc66922d573ade9d71754670c0cf58
tostu <torge.stubbe@googlemail.com>
2023-04-02 17:01:25 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <VitePwaManifest />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtWelcome />
+    </NuxtLayout>
+  </div>
+</template>
+
---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@vite-pwa/nuxt'],
+    pwa: {
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Nuxt Vite PWA',
+          short_name: 'NuxtVitePWA',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+            navigateFallback: '/',
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+          },
+          client: {
+            installPrompt: true,
+            // you don't need to include this: only for testing purposes
+            // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+            periodicSyncForUpdates: 20,
+          },
+          devOptions: {
+            enabled: true,
+            type: 'module',
+          },
+        },
+})
